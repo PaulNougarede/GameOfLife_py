094bcc4d020ed73b021d8830977f3d54325eae2a
vinciguyonneau <95126653+vinciguyonneau@users.noreply.github.com>
2023-10-29 22:36:26 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,9 +1,16 @@
 import game as Game
 import graphs.graphAlive as Graph
+import menu as Menu
+import pageAccueil as PageAccueil
 
 width, height = 800, 800
 rows, cols = 100, 100
 speed = 10
+start_game = False
+rule_choice = 0
 
-alive = Game.game(width, height, rows, cols, speed)
-Graph.graph_alive(alive)
+start_game = PageAccueil.pageAccueil()
+if start_game:
+    rule_choice = Menu.menu()
+    alive = Game.game(width, height, rows, cols, speed, rule_choice)
+    Graph.graph_alive(alive)
